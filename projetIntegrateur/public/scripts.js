8a149312bf677fb107fb46277b67bb5e32a6a2f8
Samuelfournier <sam.fournier1212@gmail.com>
2021-04-21 17:53:30 -0400

    added formulaire retour

--- projetIntegrateur/public/scripts.js
+++ projetIntegrateur/public/scripts.js
@@ -1,9 +1,9 @@
 
-
+var tableReservation
 
 $(document).ready( function () {
 
-   let tableInventaire =  $('#inventaire').DataTable({
+  let tableInventaire =  $('#inventaire').DataTable({
 
         "language": {
             "lengthMenu": "Afficher _MENU_ par pages",
@@ -26,7 +26,7 @@ $(document).ready( function () {
 
 
 
-    let tableReservation =  $('#reservation').DataTable({
+    tableReservation =  $('#reservation').DataTable({
 
       "language": {
           "lengthMenu": "Afficher _MENU_ par pages",
@@ -54,7 +54,16 @@ $(document).ready( function () {
 
 function changeQTE(id, e,currentTotal){
 
+//Assure que ce sois un nombre positif
+if(!(/^0*[1-9]\d*$/.test(e.value))){
+  alert("Vous devez entré un nombre positif")
+  return
+}
+
+
 if(currentTotal != e.value){
+
+
       //Créer l'objet
    var xhttp = new XMLHttpRequest();
 
@@ -64,8 +73,8 @@ if(currentTotal != e.value){
    // Si l'état est 4 (terminé) et le statut 200 (OK)
    if (this.readyState == 4 && this.status == 200) {
     
-     alert("Quantité changé !")
- 
+    alert("Quantité changé !")
+    location.reload()
    }
  };
  //Quel URL doit-on appeler et est-ce en GET ou en POST
@@ -80,6 +89,8 @@ if(currentTotal != e.value){
 
 
 function modifierEtat(id, e){
+
+
 
   //Créer l'objet
   var xhttp = new XMLHttpRequest();
@@ -102,6 +113,50 @@ function modifierEtat(id, e){
   xhttp.send("id="+id+"&etat="+e.value);
 
 }
+
+
+
+/*
+fonction qui lie les bon champs au formulaire dynamiquement 
+*/ 
+function retourPiece(e){
+  
+  let row = e.dataset.rownb
+  let id = e.dataset.idreservation
+  let data = tableReservation.row(row).data()
+  document.getElementById("nomEtudiant").value = data[0]
+  document.getElementById("piece").value = data[1]
+  document.getElementById("QTEEmprunter").value = data[2]
+
+}
+
+
+
+/*
+fonction qui affiche le champ description dans le retour si la piece est briser
+*/ 
+function showDescription(){
+
+  let radioOui = document.getElementById("ouiBrise")
+  let desc = document.getElementById("descriptionPieceBrise")
+  if(radioOui.checked){
+    desc.style.display = "block"
+  }
+  else{
+    desc.style.display = "none"
+  }
+}
+
+
+/*
+fonction qui reset les valeurs du formulaire de retour a chaque fin de clic de annuler
+*/
+function reset(){
+  document.getElementById("nonBrise").checked = true
+  document.getElementById("QTERetourner").value = ""
+  document.getElementById("descriptionPieceBrise").style.display = "none"
+}
+
 
 
 
