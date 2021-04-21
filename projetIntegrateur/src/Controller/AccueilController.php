15e5a8dcee7e02a306f3cf42351d7ca5d483c39f
Samuelfournier <sam.fournier1212@gmail.com>
2021-04-20 22:14:24 -0400

    Added liste reservation, with ajax

--- projetIntegrateur/src/Controller/AccueilController.php
+++ projetIntegrateur/src/Controller/AccueilController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Emprunt;
+use App\Entity\EtatEmprunt;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccueilController extends AbstractController
 {
@@ -14,10 +17,32 @@ class AccueilController extends AbstractController
     public function index(): Response
     {   
 
+        $reservationrepository = $this->getDoctrine()->getManager()->getRepository(Emprunt::class);
+        $lstReservation = $reservationrepository->lstReservation();
 
-        
+        $etatEmpruntrepository = $this->getDoctrine()->getManager()->getRepository(EtatEmprunt::class);
+        $lstEtat = $etatEmpruntrepository->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'reservations' => $lstReservation,
+            'etats' => $lstEtat,
         ]);
+        
     }
+
+
+     /**
+     * @Route("/changeEtat", name="changeEtat")
+     */
+    public function changeQTETotal(Request $r): Response
+    {
+        $info = $r->request->all();
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->getRepository(Emprunt::class)->updateEtat($info['id'],$info['etat']);
+        $em->flush();
+        
+        return new Response();
+    }
+
 }
